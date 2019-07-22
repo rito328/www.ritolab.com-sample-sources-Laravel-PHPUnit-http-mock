b467fcdcc3d73805bc3940b1b98a96a1184dad07
rito328 <ritolabo@gmail.com>
2019-07-22 22:17:48 +0900

    外部キー制約を追加

--- database/migrations/2019_07_04_125603_create_books_table.php
+++ database/migrations/2019_07_04_125603_create_books_table.php
@@ -14,9 +14,11 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
-            $table->integer('author_id');
+            $table->unsignedInteger('author_id');
+
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
         });
     }
 
